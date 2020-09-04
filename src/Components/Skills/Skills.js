e8158f38b232ad155c2a5ddb4fe760145864b616
azakimi123 <aaron.zakimi@gmail.com>
2020-09-03 23:08:45 -0600

    skills section revised

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,7 +4,8 @@ import './Skills.scss';
 
 function Skills(){
   return(
-    <div className='skills-container' id='section5'> 
+    <div className='skills-container' id='section5'>
+      <h1>Skills</h1>
       <section className='skills-icon-container'>
         <div>
           <img className='skills-icon' src='https://image.flaticon.com/icons/svg/226/226269.svg' alt='HTML'/>
@@ -37,12 +38,16 @@ function Skills(){
       </section>
       <section className='skills-icon-container2'>
         <div>
-          <img className='skills-icon' src='https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png' alt='Express.js'/>
-          <p>Express.js</p>
+          <img className='skills-icon' src='https://b.stripecdn.com/site-srv/assets/img/v3/jobs_v2/thumbnails/stripe-c7f91cf715df9fb9d2198e47de6fc3016a82795e.jpg' alt='Stripe'/>
+          <p>Stripe</p>
         </div>
         <div>
           <img className='skills-icon' src='https://iconape.com/wp-content/files/ni/64759/png/git-icon.png' alt='Git'/>
           <p>Git</p>
+        </div>
+        <div>
+          <img className='skills-icon' src='https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png' alt='Express.js'/>
+          <p>Express.js</p>
         </div>
         <div>
           <img className='skills-icon' src='https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg' alt='PostgreSQL'/>
@@ -51,10 +56,6 @@ function Skills(){
         <div>
           <img className='skills-icon' src='https://repository-images.githubusercontent.com/1272424/d1995000-0ab7-11ea-8ed3-04a082c36b0d' alt='Nodemailer'/>
           <p>Nodemailer</p>
-        </div>
-        <div>
-          <img className='skills-icon' src='https://b.stripecdn.com/site-srv/assets/img/v3/jobs_v2/thumbnails/stripe-c7f91cf715df9fb9d2198e47de6fc3016a82795e.jpg' alt='Stripe'/>
-          <p>Stripe</p>
         </div>
         <div>
           <img className='skills-icon2' src='https://www.chartjs.org/img/chartjs-logo.svg' alt='Chart.js'/>
