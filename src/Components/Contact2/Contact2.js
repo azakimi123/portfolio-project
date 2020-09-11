da466de9ab835055026b03b787290d357da4fe22
azakimi123 <aaron.zakimi@gmail.com>
2020-09-11 09:32:08 -0600

    spell check

--- src/Components/Contact2/Contact2.js
+++ src/Components/Contact2/Contact2.js
@@ -6,7 +6,7 @@ function Contact2() {
     <div className='contact2-main-page' id='section4'>
       <section className='contact-box'>
         <h1>Want to Collaborate?</h1>
-        <h3>Send me an email or text and I'll get back to yo as soon as I can</h3>
+        <h3>Send me an email or text and I'll get back to you as soon as I can</h3>
         <section>
           <p>Text/Call: (208) 243-2477</p>
           <p>Email: aaron.zakimi@mail.com</p>
