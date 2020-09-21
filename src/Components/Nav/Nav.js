7c81d9d630c097cc05b177434289d88dff36c49d
azakimi123 <aaron.zakimi@gmail.com>
2020-09-21 16:31:52 -0600

    nav dropdown

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Nav.scss';
 import Logo from '../../images/Logo.png';
 import { Link, animateScroll as scroll } from "react-scroll";
 import menuIcon from '../../images/menu_icon.svg';
 
 
-class Nav extends Component {
-  render() {
+function Nav(){
+  let [dropdownToggle, setDropdownToggle] = useState(false);
+
+
+  const handleToggle = () => {
+      setDropdownToggle(!dropdownToggle)
+  }
+
+  console.log(dropdownToggle)
     return(
       <div>
       <div className='nav'>
@@ -34,18 +41,35 @@ class Nav extends Component {
         </section>
       </div>
       <div className='nav-mobile'>
-      <section className='nav-name'>
         <Link to="section1" smooth={true} offset={-70} duration={1000}>
          <img className='nav-logo' src={Logo} alt='logo'/>
         </Link>
-      </section>
-      <section className='nav-links'>
-        <img src={menuIcon} alt='menu button' />
-      </section>
-    </div>
+        <img className='menu-button' src={menuIcon} alt='menu button' onClick={handleToggle} />
+        {dropdownToggle
+                ? (
+                        <ul>
+                            <Link to="section1" smooth={true} offset={-70} duration={1000} onClick={handleToggle}>
+                                <li>HOME</li>
+                            </Link>
+                            <Link to='section3' smooth={true} offset={0} duration={1000} onClick={handleToggle}>
+                                <li>ABOUT</li>
+                            </Link>
+                            <Link to="section5" smooth={true} offset={-85} duration={1000} onClick={handleToggle}>
+                                <li>SKILLS</li>
+                            </Link>
+                            <Link to="section2" smooth={true} offset={-70} duration={1000} onClick={handleToggle}>
+                                <li>PROJECTS</li>
+                            </Link>
+                             <Link to='section4' smooth={true} offset={-85} duration={1000} onClick={handleToggle}>
+                                <li>CONTACT</li>
+                            </Link>
+                        </ul>
+                )     
+                : null
+                }
+      </div>
     </div>
     )
-  }
 }
 
 export default Nav;
